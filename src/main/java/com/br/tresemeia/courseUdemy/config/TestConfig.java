5eb607891fd96378c61bdcce3a286888249817cb
Juliano Padoin Vieira <proprietyofkurd@hotmail.com>
2021-10-03 19:58:32 -0300

    Payment, one-to-one association

--- src/main/java/com/br/tresemeia/courseUdemy/config/TestConfig.java
+++ src/main/java/com/br/tresemeia/courseUdemy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.br.tresemeia.courseUdemy.entities.Category;
 import com.br.tresemeia.courseUdemy.entities.Order;
 import com.br.tresemeia.courseUdemy.entities.OrderItem;
+import com.br.tresemeia.courseUdemy.entities.Payment;
 import com.br.tresemeia.courseUdemy.entities.Product;
 import com.br.tresemeia.courseUdemy.entities.User;
 import com.br.tresemeia.courseUdemy.entities.enums.OrderStatus;
@@ -90,6 +91,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
